9d45201e0e6cbf990a5e0b97a867b42749332a42
ngasce <jforcesolutions@gmail.com>
2024-04-30 17:46:50 +0530

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import AboutUs from './component/AboutUs';
+import ContactUs from './component/ContactUs';
+import Home from './component/Home';
 import Navbar from './component/Navbar';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <><Navbar/></>
+    <>
+      <Router>
+          <Navbar/>
+          <Routes>
+            <Route
+              path="/home"
+              element={<Home />}
+            ></Route>
+            <Route
+              path="/contactUs"
+              element={<ContactUs />}
+            ></Route>
+            <Route
+              path="/aboutUs"
+              element={<AboutUs />}
+            ></Route>
+          </Routes>
+      </Router>
+    </>
   );
 }
 
